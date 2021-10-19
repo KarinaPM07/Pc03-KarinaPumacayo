f3c4c8cd6f6fa2a622e7bd18792f78daf6339af8
Karina <karinapm07@gmail.com>
2021-10-19 17:53:59 -0500

    Version1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pc03_KarinaPumacayo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
